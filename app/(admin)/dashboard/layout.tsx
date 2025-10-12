e3479af36e82045b75cf465830171c73926e8ccd
shurov <shurov.0.00.0@gmail.com>
2025-10-12 15:03:42 +0600

    screen width increased for the full admin dashboard design

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <Sidebar />
 
           {/* Content wrapper: on large screens add left margin to avoid overlapping the fixed sidebar */}
-          <div className="max-w-[1400px] mx-auto">
+          <div className="max-w-[1700px] mx-auto">
             <div className="lg:ml-[17rem]">{children}</div>
           </div>
         </div>
