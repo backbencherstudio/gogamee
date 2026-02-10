84e608236befe07f90038e31fc5497bc4f1efecb
najim2004 <najim10112004@gmail.com>
2026-02-10 16:18:41 +0600

    chore: Set Next.js application instances to 1 in PM2 configuration.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       cwd: process.cwd(),
-      instances: 2,
+      instances: 1,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
